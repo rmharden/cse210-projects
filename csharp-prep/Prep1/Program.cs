3befb93bc43a9388b6cfb2dcd04245e53993ae6a
rmharden <har22079@byui.edu>
2025-01-10 15:42:31 -0500

    Created basic program for Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // This creates a blank line
+        Console.WriteLine("");
+        
+        //Asks user what their first name is.
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        
+        //Asks the user what their last name is.
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        
+        // This creates a blank line
+        Console.WriteLine("");
+        
+        //Displays the sentence with the user's name.
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
     }
 }
