c5ee01f1d8d2df9aa2ef263e031e477f8ea65588
rmharden <har22079@byui.edu>
2025-01-11 12:47:10 -0500

    Created the program according to Prep2 instructions.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,10 +1,50 @@
 using System;
+using System.Runtime.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine();
+        
+        //asks the user their grade percentage.
+        Console.Write("What is your grade percentage for your class? ");
+        string valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }        
+        else 
+        {
+           letter = "F"; 
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry. You did not pass.");
+        }
     }
+
+    
 }
