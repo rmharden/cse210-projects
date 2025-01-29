26b3356ab5f1b871e7cc24ec2890c08235a28a5c
rmharden <har22079@byui.edu>
2025-01-29 16:11:26 -0500

    Added notes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,6 +1,24 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+//using System.Collections.Generic;
+//This was in our lesson, but I don't know if I was supposed
+//to put it on here. It aleady had the other two system codes.
 
+
+//What is "new" and why do we need it?
+
+//It turns out that List is a class or 
+//custom data type and we are creating 
+//a new object or instance of that class. 
+//This is actually the complete focus of 
+//this course, and beginning next week 
+//you will learn how to create your very 
+//own custom classes.
+
+//With this in mind, you will learn much 
+//more about this in coming weeks, but for 
+//now, just remember to include new before 
+//you start using a list.
 class Program
 {
     static void Main(string[] args)
