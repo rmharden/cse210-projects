b2f70178711c1f686cc4085025b0d0274cf54174
rmharden <har22079@byui.edu>
2025-01-30 09:28:29 -0500

    Added more notes.

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,12 +1,19 @@
 using System;
 
+//This is the class
 public class Job
 {
+    //This is the member variables in the class for each
+    //element.
+    //Variables begin with an underscopre and lowercase
+    //letter, such as _jobTitle.
     public string _jobTitle;
     public string _company;
     public int _startYear;
     public int _endYear;
 
+    //I think this is the method (member function) that
+    //displays the job details.
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
