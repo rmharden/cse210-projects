665382df78b65c43c15c1d7cc253af6e2ead16e9
rmharden <har22079@byui.edu>
2025-01-30 12:20:10 -0500

    Created comments in the Develop02 program.cs that includes the Rubric.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,44 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+///<summary>
+///
+///NOTES:
+///From the Rubric:
+///
+/// Principle: Journal Abstraction
+/// A class models the responsibilities of a Journal and does
+/// not include items that do not pertain to a Journal
+/// 
+/// Principle: Entry Abstraction
+/// A class models the responsibilities of an Entry and
+/// does not include itesm that do not pertain to an Entry.
+/// 
+/// Functionality: Journal Display
+/// All journal entries can be displayed along with the date
+/// and prompt associated with that entry.
+/// 
+/// Functionality: Prompt Generation
+/// A set of writing prompts is chosen from random and
+/// displayed.
+/// 
+/// Functionality: Saving
+/// The journal can be saved to a file.
+/// 
+/// Functionality: Loading
+/// The journal can be loaded from a file.
+/// 
+/// Style: Whitespace
+/// Vertical and horizontal whitespace (black lines and 
+/// indentation) is correct throughout the program.
+/// 
+/// Style: Naming Conventions
+/// Classes and methods use TitleCase. Attributes begin
+/// with an underscore and use _camelCase.
+/// 
+/// Shows creativity and exceeds core requirements
+/// The program exceeds the core requirements as explained
+/// in comments in the Program.cs,
+/// 
+///</summary>
