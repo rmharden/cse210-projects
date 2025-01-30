5c2678124e165f77b166d1af6a75cf821fd05567
rmharden <har22079@byui.edu>
2025-01-30 18:44:37 -0500

    Created comments with the journal prompts.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -19,6 +19,16 @@ class Program
 }
 
 ///<summary>
+///Prompts:
+///Who was the most interesting person I interacted with today?
+///What was the best part of my day?
+///How did I see the hand of the Lord in my life today?
+///What was the strongest emotion I felt today?
+///If I had one thing I could do over today, what would it be?
+///</summary>
+
+
+///<summary>
 ///
 ///NOTES:
 ///From the Rubric:
